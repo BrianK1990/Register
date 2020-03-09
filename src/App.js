578c7a33085ba0718e4812d1d64d1666d07847e3
Brian Kozicki <briankozicki@Brians-MacBook-Pro.local>
2020-03-09 14:08:39 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import ResgisterForm from './components/Register';
+import Messages from './components/messages';
+
+
+function App() {
+  return (
+    <div className="App">
+      <ResgisterForm />
+    </div>
+  );
+}
+
+export default App;
